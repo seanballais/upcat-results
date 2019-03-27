7339211210147339bf7ceb6cad1574f4da10cfee
Sean Francis N. Ballais <sean@seanballais.com>
2019-03-27 16:54:05 +0800

    Separated the search bar into a separate component.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 // Set the default receiver URL to the API server base URL.
 axios.defaults.baseURL = 'http://127.0.0.1:9000/';
 
-var app = new Vue({
-    el: 'article#app',
-    data: {
-        courses: [],
-        campuses: []
+var searchBar = {
+    data: function() {
+        return {
+            courses: [],
+            campuses: []
+        };
     },
-    created: function() {
-        var vm = this;
+    beforeCreate: function() {
+        var searchBar = this;
         
         axios.request({
             url: '/graphql',
@@ -18,7 +19,7 @@ var app = new Vue({
             }
         })
         .then(function(response) {
-            vm.courses = response.data.data.courses;
+            searchBar.courses = response.data.data.courses;
         });
 
         axios.request({
@@ -29,7 +30,38 @@ var app = new Vue({
             }
         })
         .then(function(response) {
-            vm.campuses = response.data.data.campuses;
+            searchBar.campuses = response.data.data.campuses;
         });
+    },
+    template: `
+        <form>
+          <div id="search-field">
+            <input type="text" id="search-box" placeholder="Type your name..." />
+            <button type="submit" id="search-button"><i class="fa fa-search"></i></button>
+          </div>
+            <div id="categories">
+              <div id="courses-dropdown" class="category-type">
+                <label>Courses</label>
+              <select name="courses">
+                <option value="0">All Courses</option>
+                <option v-for="course in courses" :value="course.id">{{ course.name }}</option>
+              </select>
+            </div>
+            <div id="campuses-dropdown" class="category-type">
+              <label>Campuses</label>
+              <select name="campuses">
+                <option value="0">All Campuses</option>
+                <option v-for="campus in campuses" :value="campus.id">{{ campus.name }}</option>
+              </select>
+            </div>
+          </div>
+        </form>
+    `
+}
+
+var app = new Vue({
+    el: 'article#app',
+    components: {
+        'search-bar': searchBar
     }
 });
