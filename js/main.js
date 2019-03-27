ea4941b6bf41fb0e7441abf3c89f7e692b218732
Sean Francis N. Ballais <sean@seanballais.com>
2019-03-27 17:59:39 +0800

    Added reactivity to the search bar.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
 axios.defaults.baseURL = 'http://127.0.0.1:9000/';
 
 var searchBar = {
+    prop: ['name_query', 'course_id', 'campus_id'],
     data: function() {
         return {
+            nameQuery: '',
+            courseID: this.course_id || 0,
+            campusID: this.campus_id || 0,
             courses: [],
             campuses: []
         };
@@ -33,24 +37,35 @@ var searchBar = {
             searchBar.campuses = response.data.data.campuses;
         });
     },
+    methods: {
+        handleNameQuery: function(e) {
+            this.$emit('input', this.nameQuery);
+        },
+        handleCourseID: function(e) {
+            this.courseID = e.target.value;
+        },
+        handleCampusID: function(e) {
+            this.campusID = e.target.value;
+        }
+    },
     template: `
         <form>
           <div id="search-field">
-            <input type="text" id="search-box" placeholder="Type your name..." />
+            <input type="text" id="search-box" placeholder="Type your name..." @input="handleNameQuery" v-model="nameQuery" />
             <button type="submit" id="search-button"><i class="fa fa-search"></i></button>
           </div>
             <div id="categories">
               <div id="courses-dropdown" class="category-type">
                 <label>Courses</label>
-              <select name="courses">
-                <option value="0">All Courses</option>
+              <select name="courses" @input="handleCourseID" v-model="courseID">
+                <option :value="0">All Courses</option>
                 <option v-for="course in courses" :value="course.id">{{ course.name }}</option>
               </select>
             </div>
             <div id="campuses-dropdown" class="category-type">
               <label>Campuses</label>
-              <select name="campuses">
-                <option value="0">All Campuses</option>
+              <select name="campuses" @input="handleCampusID" v-model="campusID">
+                <option :value="0">All Campuses</option>
                 <option v-for="campus in campuses" :value="campus.id">{{ campus.name }}</option>
               </select>
             </div>
